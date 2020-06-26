e1682e2f8c7f456efa6d1c3b5f1921fa769299ea
lanthora <nierunjie@gmail.com>
2020-06-27 00:44:06 +0800

    上班时间持久化存储

--- moyubot.py
+++ moyubot.py
@@ -1,8 +1,60 @@
 import configparser
-from lazytimer import LazyTimer
+import signal
+from time import localtime, strftime, time
+
 from telegram import Bot
 from telegram.ext import CommandHandler, Updater
-from time import time, strftime, localtime
+
+from lazytimer import LazyTimer
+
+
+def default(default_value):
+    def set_default_value(fn):
+        @wraps(fn)
+        def decorated(*args, **kwargs):
+            try:
+                ret = fn(*args, **kwargs)
+            except:
+                ret = default_value
+                logging.debug("函数 {} 使用默认返回值 {}".format(
+                    fn.__name__, default_value))
+            finally:
+                return ret
+        return decorated
+    return set_default_value
+
+
+@singleton
+class NoSQLDB(object):
+    def __init__(self):
+        self.data: dict = {}
+        self.__loaded: bool = False
+        self.__dumped: bool = False
+
+    def load(self):
+        if self.__loaded:
+            return
+        self.__loaded = True
+        self.data = self.__restore(absolute_path("database.json"))
+
+    @default({})
+    def __restore(self, path):
+        logging.info("加载数据")
+        with open(path, "r", encoding="UTF-8") as f:
+            return json.load(f)
+
+    def dump(self):
+        if(self.__dumped):
+            return
+        self.__dumped = True
+
+        logging.info("保存数据")
+        with open(absolute_path("database.json"), "w", encoding="UTF-8") as f:
+            json.dump(self.data, f, ensure_ascii=False)
+
+    def get_user_delay_db(self):
+        self.load()
+        return self.data.setdefault("user_delay", {})
 
 
 class MoyuBot:
@@ -13,8 +65,10 @@ class MoyuBot:
         self.bot = Bot(self.__token)
         self.updater = Updater(token=self.__token, use_context=True)
         self.dp = self.updater.dispatcher
-        self.dic = {}
+        self.dic = NoSQLDB().get_user_delay_db()
         self.__lazytimer = LazyTimer()
+        signal.signal(signal.SIGINT, self.sig_handler)
+        signal.signal(signal.SIGTERM, self.sig_handler)
 
     def __send_html(self, chat_id, text):
         self.bot.send_message(
@@ -42,12 +96,13 @@ class MoyuBot:
         delay: int = 0
         try:
             # 输入小时，在代码级别转化成秒
-            info = lambda cmd: [i for i in cmd.split(' ') if i != '']
+            def info(cmd): return [i for i in cmd.split(' ') if i != '']
             delay = 3600*float(info(update.message.text)[1])
-            self.dic[chat_id]=delay
+            self.dic[chat_id] = delay
+            NoSQLDB().dump()
         except IndexError:
             try:
-                delay=self.dic[chat_id]
+                delay = self.dic[chat_id]
             except KeyError:
                 self.__send_html(chat_id, "没有保存的打卡记录，请使用完整命令")
                 return
@@ -64,3 +119,7 @@ class MoyuBot:
         self.dp.add_handler(CommandHandler('checkin', self.checkin))
         self.dp.add_error_handler(self.__error)
         self.updater.start_polling()
+
+    def sig_handler(self, signal, frame):
+        NoSQLDB().dump()
+        self.updater.stop()
